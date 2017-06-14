eae43cf5a24802ba75b945c5897a1d032faef5d8
MAJESTIC123 <karthiece996@gmail.com>
2017-06-14 09:23:24 +0530

    Create leap.java

--- leap.java
+++ leap.java
@@ -2,12 +2,12 @@ class leap{
 public static void main(String[] args){
 int a=Integer.parseInt(args[0]);
 if((a%4==0)&&(a%400==0))
-System.out.println("the year is leap year");
+System.out.println("the year is a leap year");
 if((a%4==0)&&(a%100!=0))
-System.out.println("the year is leap year");
+System.out.println("the year is a leap year");
 else if(a%100==0)
-System.out.println("the year is not leap year");
+System.out.println("the year is not a  leap year");
 else
-System.out.println("the year is not leap year");
+System.out.println("the year is not a leap year");
 }
 }
